8d56739cc46e2c560b01083ad57a3b10840c9caf
PraveenJalumuru1979 <84368338+PraveenJalumuru1979@users.noreply.github.com>
2021-05-22 19:42:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ window.auth0Client = await createAuth0Client({
  domain,
  client_id,
  redirect_uri,
+  cacheLocation: "localstorage",
+        audience: "https://expenses-api", // 👈 Added
+        scope: "read:reports", // 👈 Added
 });
   
   // handle user navigation
